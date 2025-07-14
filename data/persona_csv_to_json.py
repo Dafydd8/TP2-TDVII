2fc9b2de21e7fd509eb3b419bf5a6b22b1cf9a16
Dafydd Jenkins <dafydd.a.jenkins@gmail.com>
2025-07-14 18:50:46 -0300

    persona csv to json

---
+++ data/persona_csv_to_json.py
@@ -0,0 +1,14 @@
+import csv
+import json
+
+f_in = open('persona.csv', 'r', encoding = 'utf-8')
+f_out = open('persona.json', 'w', encoding = 'utf-8')
+
+personas = []
+for line in csv.DictReader(f_in):
+    line['numero_documento'] = int(line['numero_documento'])
+    line['id_provincia'] = int(line['id_provincia'])
+    personas.append(line)
+
+print(personas)
+json.dump(personas, f_out, ensure_ascii=False, indent=2)
