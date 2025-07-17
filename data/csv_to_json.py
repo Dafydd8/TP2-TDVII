cb3af89fed25d175ff9e777d71a4d9ea1385c5fa
Dafydd Jenkins <dafydd.a.jenkins@gmail.com>
2025-07-17 20:52:15 -0300

    cargo todas las tablas como csv y json

--- data/csv_to_json.py
+++ data/csv_to_json.py
@@ -1,13 +1,24 @@
-import csv
-import json
+import converter
 
-f_in = open('persona.csv', 'r', encoding = 'utf-8')
-f_out = open('persona.json', 'w', encoding = 'utf-8')
+entities = [
+    "accededocumento",
+    "capacidad",
+    "documentodigital",
+    "efectordesalud",
+    "efectordesaludservicio",
+    "efectortercernivel",
+    "formaenvio",
+    "hce",
+    "hcecapacidad",
+    "hcegeneraobrassociales",
+    "hcegeneraprepagas",
+    "hcegenerareceta",
+    "persona",
+    "problema",
+    "provincia",
+    "servicio"
+]
 
-personas = []
-for line in csv.DictReader(f_in):
-    line['numero_documento'] = int(line['numero_documento'])
-    line['id_provincia'] = int(line['id_provincia'])
-    personas.append(line)
-
-json.dump(personas, f_out, ensure_ascii=False, indent=2)
+for entity in entities:
+    print(f"Converting {entity}...")
+    converter.convert(entity)
